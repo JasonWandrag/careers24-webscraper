5c2b0022a8c9c621ae1b5d55b6ec101151b0f7d3
JasonWandragLCA <jason@lifechoices.co.za>
2021-11-02 02:00:42 +0200

    Added comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Dependencies
 const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
@@ -5,22 +6,32 @@ const express = require("express");
 const PORT = 8000;
 const URL = "https://www.careers24.com/jobs/kw-web-developer/rmt-incl/";
 
+// Initialize Express
 const app = express();
 
+// Call Data
 axios(URL)
+  // Handle response
   .then((res) => {
     const html = res.data;
+    // Format HTML in Cheerio wrapper for JQuery syntax
     const $ = cheerio.load(html);
+
+    // Create initial data to send back
     let articles = [];
 
+    // find all elements with a class of job-card
     $(".job-card", html).each(function () {
+      // Get title and url from each job-card
       const link = $(this).find("a").attr("href");
       const title = $(this).find("h2").text();
+      // Add data to variable to push
       articles.push({
         title,
         link,
       });
     });
+    // View data in console
     console.log(articles);
   })
   .catch((err) => console.log(err));
